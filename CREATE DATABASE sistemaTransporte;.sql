9b341e8035757d4e4430eb76bb8b050c0be36723
unknown <Aprendiz@BOGDFPCGMS4144.FormacionSena.RED>
2024-04-29 10:42:00 -0500

    Create relantionship

--- CREATE DATABASE sistemaTransporte;.sql
+++ CREATE DATABASE sistemaTransporte;.sql
@@ -84,3 +84,92 @@ CREATE TABLE sistemaTransporte.Viaje(idViaje INT AUTO_INCREMENT NOT NULL,
                                     idRuta INT NOT NULL,
                                     idEmpleado INT NOT NULL,
                                     PRIMARY KEY(idViaje))ENGINE = InnoDB;
+
+
+CREATE TABLE tipoTarjeta(idTipoTarjeta INT auto_increment NOT NULL PRIMARY KEY)ENGINE = InnoDB;
+
+CREATE TABLE Troncal(idTroncal INT AUTO_INCREMENT NOT NULL PRIMARY KEY)ENGINE = InnoDB;
+                                    
+CREATE TABLE Supervisor(idSupervisor INT AUTO_INCREMENT PRIMARY KEY NOT NULL) ENGINE= InnoDB;
+
+CREATE TABLE TipoVehiculo(idTipoVehiculo INT auto_increment NOT NULL PRIMARY KEY)ENGINE =InnoDB;
+CREATE TABLE Marca(idMarca INT auto_increment NOT NULL primary KEY) ENGINE = InnoDB;
+CREATE TABLE Color(idColor INT AUTO_INCREMENT NOT NULL PRIMARY KEY) engine = InnoDB;
+
+ALTER TABLE Viaje 
+ADD codEmpleado INT NOT NULL;
+
+
+ALTER TABLE viaje
+ADD CONSTRAINT FKEmpleado 
+FOREIGN KEY(codEmpleado)
+REFERENCES Empleado(idEmpleado);
+
+ALTER TABLE viaje
+ADD CONSTRAINT FKRuta 
+FOREIGN KEY(idRuta)
+REFERENCES Ruta(idRuta);
+
+ALTER TABLE viaje
+ADD CONSTRAINT FKVehiculo 
+FOREIGN KEY(idVehiculo)
+REFERENCES Vehiculo(idVehiculo);
+
+ALTER TABLE Empleado
+ADD CONSTRAINT FKSupervisor
+FOREIGN KEY(idSupervisor)
+REFERENCES Supervisor(idSupervisor);
+
+ALTER TABLE Empleado
+ADD COLUMN idRol INT NOT NULL;
+
+ALTER TABLE Empleado
+ADD CONSTRAINT FKRol
+FOREIGN KEY(idRol)
+REFERENCES Rol(idRol);
+
+
+ALTER TABLE Incidente
+ADD CONSTRAINT FKViaje
+FOREIGN KEY(idViaje)
+REFERENCES Viaje(idViaje);
+
+ALTER TABLE Recaudo
+ADD CONSTRAINT FKTarjeta
+FOREIGN KEY(idTarjeta)
+REFERENCES Tarjeta(idTarjeta);
+
+ALTER TABLE Rol
+ADD CONSTRAINT FKEmpleado
+FOREIGN KEY(codEmpleado)
+REFERENCES Empleado(idEmpleado);
+
+ALTER TABLE Ruta
+ADD CONSTRAINT FKTroncal
+FOREIGN KEY(idTroncal)
+REFERENCES Troncal(idTroncal);
+
+ALTER TABLE Tarjeta
+ADD CONSTRAINT FKTipoTarjeta
+FOREIGN KEY(idTipoTarjeta)
+REFERENCES TipoTarjeta(idTipoTarjeta);
+
+ALTER TABLE Tarjeta
+ADD CONSTRAINT FKUsuario
+FOREIGN KEY(idUsuario)
+REFERENCES Usuario(idUsuario);
+
+ALTER TABLE Vehiculo
+ADD CONSTRAINT FKTipoVehiculo
+FOREIGN KEY(idTipoVehiculo)
+REFERENCES TipoVehiculo(idTipoVehiculo);
+
+ALTER TABLE Vehiculo
+ADD CONSTRAINT FKMarca
+FOREIGN KEY(idMarca)
+REFERENCES Marca(idMarca);
+
+ALTER TABLE Vehiculo
+ADD CONSTRAINT FKColor
+FOREIGN KEY(idColor)
+REFERENCES Color(idColor);
